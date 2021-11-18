ff41d54d3a96f3c9c1fe6c0010e95eda9b8e761f
AudricSkivée <audricskivee@gmail.com>
2021-11-18 10:47:51 +0100

    Finally make that CORS problem vanished in not a good way, will modify when on the server

--- api/index.js
+++ api/index.js
@@ -26,6 +26,14 @@ app.use(cors({
     origin:'*'
 }));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Headers", "Origin,Content-Type, Authorization, x-id, Content-Length, X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+});
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
